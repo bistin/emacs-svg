e90d637493a82a113ee642857834d77840c1b526
bistin <bistin@gmail.com>
2020-03-16 23:56:23 +0800

    use defclass

--- scale.el
+++ scale.el
@@ -1,6 +1,39 @@
+(require 'eieio)
+
 (setq lexical-binding t)
 
-(defun scaleLinear (&rest params)
+(defclass scaleLinear () ; No superclasses
+  ((domain :initarg :domain
+           :type vector
+           :documentation "domain")
+   (range :initarg :range
+          :type vector
+          :documentation "range")
+   )
+  "scale params")
+
+
+
+(cl-defmethod scale ((scale scaleLinear) (x number))
+  ""
+  (let
+      ((domain (oref scale domain))
+       (range (oref scale range))
+       )
+    (progn
+      (setq start (aref domain 0))
+      (setq end (aref domain 1))
+      (setq t1 (aref range 0))
+      (setq t2 (aref range 1))
+      (+ t1 (* (- t2 t1) (/ (- x start) (- end start) )))
+      )))
+
+
+(setq xScale (scaleLinear :domain [1 2 ] :range [4 7]))
+(scale xScale 3)
+
+
+(defun scaleLinear2 (&rest params)
   "
   Create scale.
 
